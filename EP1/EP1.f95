151ca1498ebf629237e5bc4c757d80475d8e2b6a
Lucas Magno <lmagno94@gmail.com>
2016-04-04 09:18:42 -0300

    Roda todos os testes

--- EP1/EP1.f95
+++ EP1/EP1.f95
@@ -1,37 +1,36 @@
 program EP1
     use BuscaLinear, only: maximaDescida
-    use MGH,         only: setprob, getdim, getname, getinit, f, g
+    use MGH,         only: setprob, getdim, gettries, getname, getinit, f, g, nfev, ngev
     implicit none
 
-    integer                       :: n
+    integer                       :: n, nprob, ntry, ntries
     character(len=30)             :: name
     double precision              :: factor = 1.d0
     double precision, allocatable :: x(:), x0(:)
     double precision              :: gamma, eps
 
-    call setprob(1)
-    name = getname()
-    n    = getdim()
-    x0   = getinit(factor)
-    allocate(x(n))
+    gamma = 0.1d0
+    ! eps   = epsilon(0.d0)
+    eps = 1e-3
+    print '(3A)', "nprob     ", "name                          ", "‖∇f(x₀)‖  "
+    do nprob = 1, 18
+        call setprob(nprob)
+        ntries = gettries()
+        factor = 1.d0
 
-    print *, name
-    print *, "n     =", n
-    print *, "x0    =", x
-    print *, "f(x0) =", f(x)
-    print *, "g(x0) =", g(x)
+        do ntry = 1, 1
+            name = getname()
+            n    = getdim()
+            x0   = getinit(factor)
+            allocate(x(n))
 
-    gamma = 0.5
-    eps   = 1e-6
+            call maximaDescida(x, x0, f, g, gamma, eps)
+            print '(i2, 2A, e8.2, 2i10)', nprob, "        ", name, norm2(g(x)), nfev(), ngev()
 
-    call maximaDescida(x, x0, f, g, gamma, eps)
+            deallocate(x)
+            deallocate(x0)
 
-    print *, ""
-    print *, "Mínimo:"
-    print *, "x    =", x
-    print *, "f(x) =", f(x)
-    print *, "g(x) =", g(x)
-
-    deallocate(x)
-    deallocate(x0)
+            factor = 10*factor
+        end do
+    end do
 end program EP1
